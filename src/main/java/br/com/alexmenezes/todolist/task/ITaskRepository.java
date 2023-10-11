1b5a0368cdae7008b5ba8bd4a634c96ea463f9fb
Alex Menezes <alex.menezes.one@gmail.com>
2023-10-11 13:23:56 -0300

    release tests tasks

---
+++ src/main/java/br/com/alexmenezes/todolist/task/ITaskRepository.java
@@ -0,0 +1,9 @@
+package br.com.alexmenezes.todolist.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+
+}
